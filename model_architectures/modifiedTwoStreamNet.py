febb0c78e55f0d1c48124c851955ac1fa8cf54c3
Shubhojyoti Das <shubhojyotidas800@gmail.com>
2025-07-20 17:52:12 +0000

    create new architecture and modify gitignore

---
+++ model_architectures/modifiedTwoStreamNet.py
@@ -0,0 +1,27 @@
+import torch
+from torch import nn
+
+class modifiedTwoStreamNet(nn.Module):
+    def __init__(self, FirstStream, SecondStream): 
+        
+        super(modifiedTwoStreamNet, self).__init__()
+        self.stream1 = FirstStream
+        self.stream2 = SecondStream
+        
+        self.sequential = nn.Sequential(
+            nn.Linear(in_features=1062, out_features=1062),
+            nn.Unflatten(1, (1, 1062)),
+            nn.AvgPool1d(kernel_size=2, stride=2),
+            nn.Flatten(), 
+            nn.Linear(in_features=531, out_features=2),
+            nn.Softmax(dim=1)
+        )
+
+    def forward(self, blurred_img, detailed_img):
+        f1 = self.stream1(blurred_img)
+        f2 = self.stream2(detailed_img)
+
+        x = torch.concat((f1, f2), dim=1)
+        x = self.sequential(x)
+
+        return f1, f2, x
