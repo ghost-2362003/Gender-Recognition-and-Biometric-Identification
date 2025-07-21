47e87186f03d6a5cfa14beb9093e8df2c155f03a
Shubhojyoti Das <shubhojyotidas800@gmail.com>
2025-07-21 22:49:12 +0530

    rectify the modifiedTwoStreamNet architecture

--- model_architectures/modifiedTwoStreamNet.py
+++ model_architectures/modifiedTwoStreamNet.py
@@ -17,11 +17,13 @@ class modifiedTwoStreamNet(nn.Module):
             nn.Softmax(dim=1)
         )
 
+        self.fusion = nn.Linear(in_features=1062, out_features=1062)
+
     def forward(self, blurred_img, detailed_img):
         f1 = self.stream1(blurred_img)
         f2 = self.stream2(detailed_img)
 
         x = torch.concat((f1, f2), dim=1)
-        x = self.sequential(x)
+        fusion_output = self.fusion(x)
 
-        return f1, f2, x
+        return f1, f2, fusion_output
