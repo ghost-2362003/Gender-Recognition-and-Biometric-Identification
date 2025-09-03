d4454e21c4092fe3a8d37a1906474215866b583f
LuxVeritas <shubhojyotidas800@gmail.com>
2025-09-03 13:11:42 +0530

    push experimental svm_gender_classifier

--- model_architectures/svm_gender_classifier.py
+++ model_architectures/svm_gender_classifier.py
@@ -1,7 +1,10 @@
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import LabelEncoder
 import pandas as pd
+import numpy as np
 
 # creating the dataframes
 train_data = pd.read_csv('dataset/train_features.csv')
@@ -15,17 +18,24 @@ train_data['Label'] = train_data['Label'].apply(lambda x: 'F' if x == '[0]' else
 X_train, X_test, y_train, y_test = train_test_split(train_data.drop(columns=['Label']),
                                                     train_data['Label'], random_state=42,
                                                     shuffle=True, stratify=train_data['Label'])
+le = LabelEncoder()
+y_train = le.fit_transform(y_train)
+y_test = le.fit_transform(y_test)
 
 pca = PCA(n_components=0.99)
 pca_train = pca.fit_transform(X_train)
 pca_train = pd.DataFrame(pca_train)
 pca_test = pca.transform(X_test)
 
-# creating the SVM classifier
-svm_classifier = SVC(kernel='sigmoid',
-                     C=1.0, verbose=True)
-svm_classifier.fit(pca_train, y_train)
+svm_classifier = SVC(kernel='poly')
+param_grid = {
+    'gamma': [0.001, 0.01, 0.1, 1, 5, 10],
+    'C': [0.001, 0.01, 0.1, 1, 5, 10]
+}
+grid_search = GridSearchCV(svm_classifier, param_grid=param_grid, cv=10,
+                           scoring='neg_root_mean_squared_error')
+grid_search.fit(pca_train, y_train)
 
-# making predictions
-svm_preds = svm_classifier.predict(pca_test)
-svm_preds
+print("Best parameters:", grid_search.best_params_)
+print("Best CV score:", grid_search.best_score_)
+print("Best estimator:", grid_search.best_estimator_)
