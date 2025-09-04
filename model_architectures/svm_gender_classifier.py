a2cb9c5da1fbdb4fb98221ace8b5b4ec66f8cb21
LuxVeritas <shubhojyotidas800@gmail.com>
2025-09-05 00:01:06 +0530

    add modifications to svm and visuals

--- model_architectures/svm_gender_classifier.py
+++ model_architectures/svm_gender_classifier.py
@@ -1,10 +1,9 @@
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
-from sklearn.model_selection import GridSearchCV
-from sklearn.preprocessing import LabelEncoder
 import pandas as pd
-import numpy as np
+from joblib import load, dump
+from pre_processing.visuals import generate_heatmap, generate_reports
 
 # creating the dataframes
 train_data = pd.read_csv('dataset/train_features.csv')
@@ -18,24 +17,20 @@ train_data['Label'] = train_data['Label'].apply(lambda x: 'F' if x == '[0]' else
 X_train, X_test, y_train, y_test = train_test_split(train_data.drop(columns=['Label']),
                                                     train_data['Label'], random_state=42,
                                                     shuffle=True, stratify=train_data['Label'])
-le = LabelEncoder()
-y_train = le.fit_transform(y_train)
-y_test = le.fit_transform(y_test)
 
 pca = PCA(n_components=0.99)
 pca_train = pca.fit_transform(X_train)
 pca_train = pd.DataFrame(pca_train)
 pca_test = pca.transform(X_test)
 
-svm_classifier = SVC(kernel='poly')
-param_grid = {
-    'gamma': [0.001, 0.01, 0.1, 1, 5, 10],
-    'C': [0.001, 0.01, 0.1, 1, 5, 10]
-}
-grid_search = GridSearchCV(svm_classifier, param_grid=param_grid, cv=10,
-                           scoring='neg_root_mean_squared_error')
-grid_search.fit(pca_train, y_train)
+'''
+svm_classifier = SVC(C=9, gamma=0.001, kernel='rbf')
+svm_classifier.fit(pca_train, y_train)
+dump(svm_classifier, 'model_files/svm_classifier.jobllib')
+'''
 
-print("Best parameters:", grid_search.best_params_)
-print("Best CV score:", grid_search.best_score_)
-print("Best estimator:", grid_search.best_estimator_)
+svm_classifier = load('model_files/svm_classifier.jobllib')
+y_pred = svm_classifier.predict(pca_test)
+
+generate_heatmap(y_test, y_pred)
+generate_reports(y_pred, y_test, svm_classifier.decision_function(pca_test))
--- pre_processing/visuals.py
+++ pre_processing/visuals.py
@@ -1,1 +1,21 @@
-from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix, roc_auc_score
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def generate_heatmap(y_true, y_pred):
+    cm = confusion_matrix(y_true, y_pred)
+    
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(cm, annot=True, fmt='d', 
+                       cmap='Purples', xticklabels=['M', 'F'], 
+                       yticklabels=['M', 'F'])
+    plt.title('confusion matrix')
+    plt.tight_layout()
+    plt.show() 
+    
+def generate_reports(y_pred, y_true, proba):
+    report = classification_report(y_pred, y_true)
+    accuracy = accuracy_score(y_true, y_pred)
+    curves = roc_auc_score(y_true, proba)
+    
+    return report, accuracy, curves
